16100dd60272ef8e7e5599a2ade0de8e8973132a
faridseb <153421992+faridseb@users.noreply.github.com>
2024-09-04 06:48:32 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -80,6 +80,7 @@ function ready(){
 
     updatotal();
     loadCartitems();
+    saveCartitems() ;
 }
 
 
@@ -94,7 +95,7 @@ function removeitem(event){
 function quantitychange(event){
     var input = event.target ;
     if(isNaN(input.value) || input.value <= 0){
-        input.value = 1
+        input.value = 1 ;
     }
     updatotal() ;
     saveCartitems();
